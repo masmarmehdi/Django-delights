f7f58cc03fcff5e87957809bce35769825a73475
Mehdi Masmar <masmar507@gmail.com>
2022-11-15 01:24:59 +0100

    Registering models to the admin site with custom display

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
 
-# Register your models here.
+@admin.register(Ingredient)
+class IngeredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'quantity', 'unit', 'unit_price')
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'price')
+
+@admin.register(RecipeRequirement)
+class RecipeRequirementAdmin(admin.ModelAdmin):
+    list_display = ('menu_item', 'ingredient', 'quantity')
+
+@admin.register(Purchase)
+class PurchaseAdmin(admin.ModelAdmin):
+    list_display = ('menu_item', 'timestamp')
