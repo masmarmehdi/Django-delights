58b93b4aa92b8b66cc55c1492743ca61c00b3030
Mehdi Masmar <masmar507@gmail.com>
2022-11-15 15:53:11 +0100

    adding necessary urls

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
-
+from .views import *
 urlpatterns = [
-
+    path('ingredients/', IngredientListView.as_view()),
+    path('menu-items/', MenuItemListView.as_view()),
+    path('recipe-requirements/', RecipeRequirementListView.as_view()),
+    path('purchases/', PurchaseListView.as_view())
 ]
