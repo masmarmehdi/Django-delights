3dabed8778b67a3403cbc0a8c0a7984aab6beda6
Mehdi Masmar <masmar507@gmail.com>
2022-11-16 14:01:01 +0100

    adding auth path + purchases path

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import *
+
 urlpatterns = [
+    path('', HomeListView.as_view()),
     path('ingredients/', IngredientListView.as_view()),
     path('menu-items/', MenuItemListView.as_view()),
     path('recipe-requirements/', RecipeRequirementListView.as_view()),
-    path('purchases/', PurchaseListView.as_view())
+    path('purchases/', PurchaseListView.as_view()),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginInterfaceView.as_view(), name='login'),
+    path('logout/', LogoutInterfaceView.as_view(), name='logout')
 ]
