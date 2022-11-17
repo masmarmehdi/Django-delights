559ea6dc4f5f54c9c2fddbe62eae26a0c237c315
Mehdi Masmar <masmar507@gmail.com>
2022-11-17 16:23:59 +0100

    adding base directory for static folder

--- djangoDelights/settings.py
+++ djangoDelights/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'djangoDelights.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'static/templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,7 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
+LOGIN_REDIRECT_URL = "/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
