03522e8289b76241448f42c7a427c195c588c9bd
Mehdi Masmar <masmar507@gmail.com>
2022-11-17 02:07:06 +0100

    adding new urls + naming them

--- inventory/urls.py
+++ inventory/urls.py
@@ -2,12 +2,14 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', HomeListView.as_view()),
-    path('ingredients/', IngredientListView.as_view()),
-    path('menu-items/', MenuItemListView.as_view()),
-    path('recipe-requirements/', RecipeRequirementListView.as_view()),
-    path('purchases/', PurchaseListView.as_view()),
+    path('', HomeListView.as_view(), name='home'),
+    path('ingredients/', IngredientListView.as_view(), name='ingredients'),
+    path('menu-items/', MenuItemListView.as_view(), name='menu-items'),
+    path('recipe-requirements/', RecipeRequirementListView.as_view(), name='recipe-requirements'),
+    path('purchases/', PurchaseListView.as_view(), name='purchases'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginInterfaceView.as_view(), name='login'),
-    path('logout/', LogoutInterfaceView.as_view(), name='logout')
+    path('logout/', LogoutInterfaceView.as_view(), name='logout'),
+    path('ingredients/create', IngredientCreateView.as_view(), name='create-ingredient'),
+    path('ingredients/edit/<int:ingredient_id>', IngredientUpdate.as_view(), name='update-ingredient')
 ]
