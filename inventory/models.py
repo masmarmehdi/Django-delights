27a452b973758adf58a80a60ce7b6268b81bbf78
Mehdi Masmar <masmar507@gmail.com>
2022-11-22 22:14:48 +0100

    adding functions: available and enough

--- inventory/models.py
+++ inventory/models.py
@@ -7,12 +7,15 @@ class Ingredient(models.Model):
     unit_price = models.FloatField(default=0)
 
     def __str__(self):
-        return f'{self.name}' 
+        return f'{self.name}'
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=100)
     price = models.FloatField(default=0)
 
+    def available(self):
+        return all(X.enough() for X in self.reciperequirement_set.all())
+    
     def __str__(self):
         return f'{self.title}'
 
@@ -21,11 +24,14 @@ class RecipeRequirement(models.Model):
     ingredient = models.ForeignKey(Ingredient, on_delete=models.DO_NOTHING)
     quantity = models.FloatField(default=0)
 
+    def enough(self):
+        return self.quantity <= self.ingredient.quantity
+    
     def __str__(self):
         return f'{self.menu_item}'
 
 class Purchase(models.Model):
-    menu_item = models.ForeignKey(MenuItem, on_delete=models.DO_NOTHING)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now=True)
 
     def __str__(self):
