1ca1cdcdd69ce7dfedee485b8c689e35a12b7cf2
Mehdi Masmar <masmar507@gmail.com>
2022-11-22 22:12:16 +0100

    adding urls for ingredient, menu item, recipe requirement and purchases

--- inventory/urls.py
+++ inventory/urls.py
@@ -12,5 +12,12 @@ urlpatterns = [
     path('logout/', LogoutInterfaceView.as_view(), name='logout'),
     path('ingredients/create', IngredientCreateView.as_view(), name='create-ingredient'),
     path('ingredients/edit/<int:pk>', IngredientUpdateView.as_view(), name='update-ingredient'),
-    path('ingredients/delete/<int:pk>', IngredientDeleteView.as_view(), name='delete-ingredient')
+    path('ingredients/delete/<int:pk>', IngredientDeleteView.as_view(), name='delete-ingredient'),
+    path('menu-items/create', MenuItemCreateView.as_view(), name='create-menu_items'),
+    path('menu-items/edit/<int:pk>', MenuItemUpdateView.as_view(), name='update-menu_items'),
+    path('menu-items/delete/<int:pk>', MenuItemDeleteView.as_view(), name='delete-menu_items'),
+    path('recipe-requirements/create/', RecipeRequirementCreateView.as_view(), name='create-recipe_requirement'),
+    path('recipe-requirements/edit/<int:pk>', RecipeRequirementUpdateView.as_view(), name='update-recipe_requirement'),
+    path('recipe-requirements/delete/<int:pk>', RecipeRequirementDeleteView.as_view(), name='delete-recipe_requirement'),
+    path('purchases/create', PurchaseCreateView.as_view(), name='create-purchase')
 ]
